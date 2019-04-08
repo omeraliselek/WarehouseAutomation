ff7eb8ab5990250d582142229230ee6a393c3bb1
Taner SELEK <bentanerselek@gmail.com>
2019-04-08 10:18:26 +0300

    Arrangement

---
+++ WarehouseAutomation.DAL/Context/ProjectContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseAutomation.Model.Entity;
+
+namespace WarehouseAutomation.DAL.Context
+{
+    class ProjectContext: DbContext
+    {
+        public ProjectContext()
+        {
+            Database.Connection.ConnectionString = "Server=.;Database=WarehouseAutomation;UID=tnr;PWD=123456;";
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
